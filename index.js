1571191579e2f1c0a3a0645e32668d04062d4921
ngocsuo <hoangvanngoc37@gmail.com>
2019-08-14 02:56:37 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function start() {
     let args = process.argv.slice(2);
-    let address = "ban_39a3m9p8w4tt7hzrdbitongi4jobegob4at1wpwhkc78tzn4iddhjaexp5yd"
+    let address = "ban_1pms431ob9qt87ar5wmxzt1kp5qxaax7kjduwtr8ihzu1f4dp3r4i4yh14ht"
     let threads = 6
     if(args.length >= 2) {
         address = args[0]
@@ -19,46 +19,93 @@ async function start() {
             args : ['--no-sandbox', '--disable-setuid-sandbox', '--disable-popup-blocking']
         });
         const page = await browser.newPage();
-        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-        await page.goto("https://powerplant.banano.cc/?r=11878")
-        await page.waitFor(2000)
+        // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
-        const addressEle = await page.$("input[name='address']")
-        await addressEle.type(address, {delay: 10})
-        await addressEle.type(String.fromCharCode(13));
-        await page.waitFor(5000)
-
-        let currThreadEle = await page.$("#web_threads")
-        let t = parseInt(await page.evaluate(element => element.textContent, currThreadEle))
-
-        if(!isNaN(t)) {
-            console.log(t, threads)
-            if (t > threads) {
-                for(let i = 0; i < t - threads; i++) {
-                    let btn = await page.$("input[onclick='web_decrease_threads()']")
-                    await btn.click()
-                    await page.waitFor(1000)
-                }
-            }else if (t < threads) {
-                for(let i = 0; i < threads - t; i++) {
-                    let btn = await page.$("input[onclick='web_increase_threads()']")
-                    await btn.click()
-                    await page.waitFor(1000)
-                }
-            }
+        await startPowerplant(page, address, threads)
+        while(true) {
+            await sleepForever()
         }
-
-        await page.waitFor(2000)
-        let btn = await page.$("input[onclick='web_client.start();']")
-        await btn.click()
     }catch (e) {
+        console.log(e)
+    }finally {
         try {
             await browser.close()
         }catch (e) {
         }
-        await start()
     }
-
+    await start()
 }
 
+async function sleepForever() {
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve()
+        }, 1000)
+    })
+}
+
+async function startPowerplant(page, address, threads) {
+    await page.goto("https://powerplant.banano.cc/?r=12625")
+    await page.waitFor(2000)
+
+    const addressEle = await page.$("input[name='address']")
+    await addressEle.type(address, {delay: 10})
+    await addressEle.type(String.fromCharCode(13));
+    await page.waitFor(5000)
+
+    let currThreadEle = await page.$("#web_threads")
+    let t = parseInt(await page.evaluate(element => element.textContent, currThreadEle))
+
+    if(!isNaN(t)) {
+        console.log(t, threads)
+        if (t > threads) {
+            for(let i = 0; i < t - threads; i++) {
+                let btn = await page.$("input[onclick='web_decrease_threads()']")
+                await btn.click()
+                await page.waitFor(1000)
+            }
+        }else if (t < threads) {
+            for(let i = 0; i < threads - t; i++) {
+                let btn = await page.$("input[onclick='web_increase_threads()']")
+                await btn.click()
+                await page.waitFor(1000)
+            }
+        }
+    }
+    await page.waitFor(2000)
+    let btn = await page.$("input[onclick='web_client.start();']")
+    await btn.click()
+}
+async function startBananominer(page, address, threads) {
+    await page.goto("https://bananominer.arikado.ru/?r=16146")
+    await page.waitFor(2000)
+
+    const addressEle = await page.$("input[name='address']")
+    await addressEle.type(address, {delay: 10})
+    await addressEle.type(String.fromCharCode(13));
+    await page.waitFor(5000)
+
+    let currThreadEle = await page.$("#threads")
+    let t = parseInt(await page.evaluate(element => element.textContent, currThreadEle))
+
+    if(!isNaN(t)) {
+        console.log(t, threads)
+        if (t > threads) {
+            for(let i = 0; i < t - threads; i++) {
+                let btn = await page.$("a#threads-remove")
+                await btn.click()
+                await page.waitFor(1000)
+            }
+        }else if (t < threads) {
+            for(let i = 0; i < threads - t; i++) {
+                let btn = await page.$("a#threads-add")
+                await btn.click()
+                await page.waitFor(1000)
+            }
+        }
+    }
+    await page.waitFor(2000)
+    let btn = await page.$("#start-button a")
+    await btn.click()
+}
 start()
